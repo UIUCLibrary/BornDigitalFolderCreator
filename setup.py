3658e4eb8c36a3ec44830fa3518871c08498caa9
Henry Borchers <henryborchers@yahoo.com>
2016-09-01 16:42:31 -0500

    as

--- setup.py
+++ setup.py
@@ -1,21 +1,6 @@
 from setuptools import setup
 
-import sys
-from setuptools.command.test import test as TestCommand
 
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        # self.pytest_args.append("--doctest-modules")
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 setup(
     name='CreateFolders',
@@ -23,8 +8,6 @@ setup(
     packages=['BDCreateFolders', "tests"],
     entry_points={'console_scripts': ['createfolders = BDCreateFolders.__main__:main']},
     test_suite="tests",
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
     url='https://github.com/UIUCLibrary/BornDigitalFolderCreator',
     license='',
     author='Henry Borchers',
