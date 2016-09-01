92db6d5b973574dca73c8e2377d65129eb65b2de
Henry Borchers <henryborchers@yahoo.com>
2016-09-01 16:52:05 -0500

    as

--- setup.py
+++ setup.py
@@ -5,13 +5,11 @@ from setuptools import setup
 setup(
     name='CreateFolders',
     version='0.0.2',
+    description='Generates folders based on a CSV exported from MEDUSA',
     packages=['BDCreateFolders', "tests"],
-    entry_points={'console_scripts': ['createfolders = BDCreateFolders.__main__:main']},
-    test_suite="tests",
     url='https://github.com/UIUCLibrary/BornDigitalFolderCreator',
-    license='',
     author='Henry Borchers',
     author_email='hborcher@illinois.edu',
-    description='Generates folders based on a CSV exported from MEDUSA',
-    zip_safe=False
+    entry_points={'console_scripts': ['createfolders = BDCreateFolders.__main__:main']},
+    license='',
 )
